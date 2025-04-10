5dc3f8b388354a3a30c1e95341f4af6be2cf64a6
Tuananh <tuananh48218@gmail.com>
2025-04-10 22:09:49 +0700

    Refactor HomeController to use dynamic response based on TEST value

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,14 +1,22 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
+@RequiredArgsConstructor
 public class HomeController {
+
+    @Value("${TEST}")
+    private String test;
 
     @GetMapping
     public String home() {
-        return "Hello";
+        return test == null ? "hello" : test;
     }
 
 }
